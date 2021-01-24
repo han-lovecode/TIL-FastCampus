5f34d4f634e88644ec732d979b71a067b0481c12
han-lovecode <mcdreamy531@gmail.com>
2021-01-25 00:36:09 +0900

    github_example(new) finished

---
+++ github_example(new)/main.js
@@ -0,0 +1,29 @@
+(function (window, document) {
+    'use strict';
+  
+    const $toggles = document.querySelectorAll('.toggle'); // NodeList(유사배열)
+    const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
+    
+    $toggleBtn.addEventListener('click', function () {
+      toggleElements();
+    });
+  
+    window.addEventListener('resize', function () {
+      if (window.innerWidth > 1024) {
+        offElements();
+      }
+    });
+  
+    function toggleElements() {
+      [].forEach.call($toggles, function (toggle) {
+        toggle.classList.toggle('on');
+      });
+    }
+  
+    function offElements() {
+      [].forEach.call($toggles, function (toggle) {
+        toggle.classList.remove('on');
+      });
+    }
+  })(window, document);
+// 전체영역을 더럽히지 않도록 모듈화
