c9a06922fca4c7299c8584465e4428777af2ffbc
han-lovecode <mcdreamy531@gmail.com>
2021-02-06 09:00:44 +0900

    fc-assign8 submitted

---
+++ fc-assign/assign8/8/src/exam/1.js
@@ -0,0 +1,23 @@
+/*
+1. 수박수박수박수박수박수? (10점)
+길이가 n이고, 수박수박수박수...와 같은 패턴을 유지하는 문자열을 리턴하는 함수 waterMelon을 만들어주세요.
+
+**출력예시**
+* n이 3일 때 '수박수'
+* n이 4일 때 '수박수박'
+*/
+
+function waterMelon(n) {
+  let answer = ''  
+   for (let i = 0; i < n; i++) {
+      if (i % 2 === 0) { 
+        answer += '수' 
+      } else { 
+        answer += '박'
+    }
+  }   
+  return answer
+}
+
+console.log(waterMelon(3)); // 수박수
+console.log(waterMelon(4)); // 수박수박
